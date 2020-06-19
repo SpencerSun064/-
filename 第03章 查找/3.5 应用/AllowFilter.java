c4e9379af13a48a92462ce348361652f91e91876
Spencer.Sun <darkpurple01@sina.com>
2020-06-19 08:33:01 -0500

    Add files via upload

---
+++ 第03章 查找/3.5 应用/AllowFilter.java
@@ -0,0 +1,23 @@
+public class AllowFilter {  
+
+    // Do not instantiate.
+    private AllowFilter() { }
+
+    public static void main(String[] args) {
+        SET<String> set = new SET<String>();
+
+        // read in strings and add to set
+        In in = new In(args[0]);
+        while (!in.isEmpty()) {
+            String word = in.readString();
+            set.add(word);
+        }
+
+        // read in string from standard input, printing out all exceptions
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (set.contains(word))
+                StdOut.println(word);
+        }
+    }
+}
