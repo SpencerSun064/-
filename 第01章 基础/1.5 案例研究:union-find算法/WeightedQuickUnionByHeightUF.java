90793dbc7ea2b48462dcab35afb46373730071ae
Spencer.Sun <darkpurple01@sina.com>
2021-05-13 13:12:51 +0800

    Add files via upload

---
+++ 第01章 基础/1.5 案例研究:union-find算法/WeightedQuickUnionByHeightUF.java
@@ -0,0 +1,117 @@
+public class WeightedQuickUnionByHeightUF {
+    private int[] parent;   // parent[i] = parent of i
+    private int[] height;   // height[i] = height of subtree rooted at i
+    private int count;  // number of components
+
+    /**
+     * Initializes an empty union-find data structure with
+     * {@code n} elements {@code 0} through {@code n-1}. 
+     * Initially, each elements is in its own set.
+     *
+     * @param  n the number of elements
+     * @throws IllegalArgumentException if {@code n < 0}
+     */
+    public WeightedQuickUnionByHeightUF(int n) {
+        count = n;
+        parent = new int[n];
+        height = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            height[i] = 0;
+        }
+    }
+
+    /**
+     * Returns the number of sets.
+     *
+     * @return the number of sets (between {@code 1} and {@code n})
+     */
+    public int count() {
+        return count;
+    }
+  
+    /**
+     * Returns the canonical element of the set containing element {@code p}.
+     *
+     * @param  p an element
+     * @return the canonical element of the set containing {@code p}
+     * @throws IllegalArgumentException unless {@code 0 <= p < n}
+     */
+    public int find(int p) {
+        validate(p);
+        while (p != parent[p])
+            p = parent[p];
+        return p;
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));
+        }
+    }
+
+    /**
+     * Returns true if the two elements are in the same set.
+     * 
+     * @param  p one element
+     * @param  q the other element
+     * @return {@code true} if {@code p} and {@code q} are in the same set;
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     * @deprecated Replace with two calls to {@link #find(int)}.
+     */
+    @Deprecated
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    /**
+     * Merges the set containing element {@code p} with the 
+     * the set containing element {@code q}.
+     *
+     * @param  p one element
+     * @param  q the other element
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+     */
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j)
+            return;
+
+        // make shorter root point to taller one
+        if      (height[i] < height[j]) parent[i] = j;
+        else if (height[i] > height[j]) parent[j] = i;
+        else {
+            parent[j] = i;
+            height[i]++;
+        }
+        count--;
+    }
+
+    /**
+     * Reads an integer {@code n} and a sequence of pairs of integers
+     * (between {@code 0} and {@code n-1}) from standard input, where each integer
+     * in the pair represents some element;
+     * if the elements are in different sets, merge the two sets
+     * and print the pair to standard output.
+     * 
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        WeightedQuickUnionByHeightUF uf = new WeightedQuickUnionByHeightUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.find(p) == uf.find(q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+}
