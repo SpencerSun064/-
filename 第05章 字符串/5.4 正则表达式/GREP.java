ec2e43153080bf93fce14d5a4bfb5feab44701be
Spencer.Sun <darkpurple01@sina.com>
2022-03-22 19:30:05 +0800

    Add files via upload

---
+++ 第05章 字符串/5.4 正则表达式/GREP.java
@@ -0,0 +1,24 @@
+public class GREP {
+
+    private GREP() { }
+
+    /**
+     * Interprets the command-line argument as a regular expression
+     * (supporting closure, binary or, parentheses, and wildcard)
+     * reads in lines from standard input; writes to standard output
+     * those lines that contain a substring matching the regular
+     * expression.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) { 
+        String regexp = "(.*" + args[0] + ".*)";
+        NFA nfa = new NFA(regexp);
+        while (StdIn.hasNextLine()) { 
+            String line = StdIn.readLine();
+            if (nfa.recognizes(line)) {
+                StdOut.println(line);
+            }
+        }
+    } 
+} 
